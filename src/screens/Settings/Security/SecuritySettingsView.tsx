e7b592d65dd3a460b432c7b4b20625bdce472962
N3TC4T <netcat.av@gmail.com>
2020-04-03 16:40:42 +0200

    fix: fallback

--- src/screens/Settings/Security/SecuritySettingsView.tsx
+++ src/screens/Settings/Security/SecuritySettingsView.tsx
@@ -228,14 +228,16 @@ class SecuritySettingsView extends Component<Props, State> {
                         </View>
                     </TouchableOpacity>
 
-                    <View style={styles.row}>
-                        <View style={[AppStyles.flex3]}>
-                            <Text style={styles.label}>{Localize.t('settings.biometricAuthentication')}</Text>
+                    {isSensorAvailable && (
+                        <View style={styles.row}>
+                            <View style={[AppStyles.flex3]}>
+                                <Text style={styles.label}>{Localize.t('settings.biometricAuthentication')}</Text>
+                            </View>
+                            <View style={[AppStyles.rightAligned, AppStyles.flex1]}>
+                                <Switch checked={biometricEnabled} onChange={this.biometricMethodChange} />
+                            </View>
                         </View>
-                        <View style={[AppStyles.rightAligned, AppStyles.flex1]}>
-                            <Switch checked={biometricEnabled} onChange={this.biometricMethodChange} />
-                        </View>
-                    </View>
+                    )}
                 </ScrollView>
             </View>
         );
