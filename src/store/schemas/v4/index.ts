babd1a6cc7040bfd7f6c5124d4f2a81246cd5b66
N3TC4T <netcat.av@gmail.com>
2020-06-18 16:22:59 +0200

    chore: schema v4 [pre]

--- src/store/schemas/v4/index.ts
+++ src/store/schemas/v4/index.ts
@@ -2,14 +2,14 @@ import ProfileSchema from '@store/schemas/v1/profile';
 import ContactSchema from '@store/schemas/v1/contact';
 import AccountSchema from '@store/schemas/v2/account';
 // changed
-import CoreSchema from '@store/schemas/v3/core';
+import CoreSchema from '@store/schemas/v4/core';
 import CounterPartySchema from '@store/schemas/v4/counterParty';
 import CurrencySchema from '@store/schemas/v4/currency';
 import TrustLineSchema from '@store/schemas/v4/trustLine';
 
 /* Migration ==================================================================== */
 export const migration = (oldRealm: any, newRealm: any) => {
-    // CoreSchema.migration(oldRealm, newRealm);
+    CoreSchema.migration(oldRealm, newRealm);
     CounterPartySchema.migration(oldRealm, newRealm);
     CurrencySchema.migration(oldRealm, newRealm);
     TrustLineSchema.migration(oldRealm, newRealm);
