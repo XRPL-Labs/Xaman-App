44ab15db1432ea9c68bcb838b9f0043690f63db3
N3TC4T <netcat.av@gmail.com>
2020-10-30 10:36:53 +0100

    fix: filter incoming trustlines

--- src/services/LedgerService.ts
+++ src/services/LedgerService.ts
@@ -425,7 +425,11 @@ class LedgerService extends EventEmitter {
                     // ignore incoming trustline
                     let filteredLines = flatMap(
                         omitBy(lines, (l: any) => {
-                            return Number(l.balance) < 0 || (Number(l.limit) === 0 && Number(l.limit_peer) > 0);
+                            return (
+                                Number(l.balance) < 0 ||
+                                (Number(l.limit) === 0 && Number(l.limit_peer) > 0) ||
+                                (Number(l.balance) === 0 && Number(l.limit) === 0 && Number(l.limit_peer) === 0)
+                            );
                         }),
                     );
 
