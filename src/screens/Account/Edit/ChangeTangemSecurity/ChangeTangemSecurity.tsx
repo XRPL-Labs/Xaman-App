1eca378acf3be0fb3d8fc86e613ef13c9b444eb9
N3TC4T <netcat.av@gmail.com>
2021-03-31 15:28:31 +0200

    fix: show alert after card security change

--- src/screens/Account/Edit/ChangeTangemSecurity/ChangeTangemSecurity.tsx
+++ src/screens/Account/Edit/ChangeTangemSecurity/ChangeTangemSecurity.tsx
@@ -11,6 +11,7 @@ import RNTangemSdk, { Card } from 'tangem-sdk-react-native';
 import { AppScreens } from '@common/constants';
 
 import { Navigator } from '@common/helpers/navigator';
+import { Prompt } from '@common/helpers/interface';
 
 import { AccountRepository } from '@store/repositories';
 import { AccountSchema } from '@store/schemas/latest';
@@ -76,6 +77,7 @@ class ChangeTangemSecurityView extends Component<Props, State> {
     };
 
     onSuccessChange = (isPin2Default: boolean) => {
+        const { currentSecurity } = this.state;
         const { account } = this.props;
 
         AccountRepository.update({
@@ -86,7 +88,18 @@ class ChangeTangemSecurityView extends Component<Props, State> {
             }),
         });
 
-        Navigator.pop();
+        Prompt(
+            Localize.t('global.success'),
+            Localize.t('account.cardSecuritySuccessfullyChangedTo', { security: currentSecurity }),
+            [
+                {
+                    onPress: () => {
+                        Navigator.pop();
+                    },
+                },
+            ],
+            { type: 'default' },
+        );
     };
 
     removePasscode = () => {
