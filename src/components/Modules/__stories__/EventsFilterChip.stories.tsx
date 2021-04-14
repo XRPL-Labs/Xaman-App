e2580af9d7274ebdd901d80b34dc05ebc914a214
N3TC4T <netcat.av@gmail.com>
2021-04-14 13:31:49 +0200

    chore: refactore storyboard stories

--- src/components/Modules/__stories__/EventsFilterChip.stories.tsx
+++ src/components/Modules/__stories__/EventsFilterChip.stories.tsx
@@ -1,8 +1,8 @@
-/* eslint-disable spellcheck/spell-checker */
 import React from 'react';
-import { View } from 'react-native';
 import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react-native';
+
+import { withBackground } from '../../../../storybook/decoration';
 
 import { EventsFilterChip } from '../EventsFilterChip';
 
@@ -15,6 +15,5 @@ export const filters = {
 };
 
 storiesOf('EventsFilterChip', module)
-    // eslint-disable-next-line react-native/no-inline-styles
-    .addDecorator((storyFn: any) => <View style={{ flex: 1, justifyContent: 'center' }}>{storyFn()}</View>)
+    .addDecorator(withBackground)
     .add('default', () => <EventsFilterChip filters={filters} onRemovePress={action('onPress')} />);
--- src/components/Modules/__stories__/RecipientElement.stories.tsx
+++ src/components/Modules/__stories__/RecipientElement.stories.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable spellcheck/spell-checker */
 import React from 'react';
-import { View } from 'react-native';
 import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react-native';
+
+import { withBackground } from '../../../../storybook/decoration';
 
 import { RecipientElement } from '../RecipientElement';
 
@@ -14,8 +15,7 @@ export const recipientData = {
 };
 
 storiesOf('RecipientElement', module)
-    // eslint-disable-next-line react-native/no-inline-styles
-    .addDecorator((storyFn: any) => <View style={{ flex: 1, justifyContent: 'center' }}>{storyFn()}</View>)
+    .addDecorator(withBackground)
     .add('default', () => <RecipientElement recipient={{ ...recipientData }} onPress={action('onPress')} />)
     .add('WithSource', () => (
         <RecipientElement
