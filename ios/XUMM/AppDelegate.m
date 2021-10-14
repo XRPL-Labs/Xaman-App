5a1ee53e43666895e38c52c71cce2a9d71a62a52
N3TC4T <netcat.av@gmail.com>
2021-10-14 16:38:40 +0200

    fix(ios): snapshot privacy is not hiding overlays

--- ios/XUMM/AppDelegate.m
+++ ios/XUMM/AppDelegate.m
@@ -11,7 +11,7 @@
 
 
 #import "Libs/Notification/LocalNotification.h"
-//#import "Libs/Common/InAppPurchase.h"
+
 
 @implementation AppDelegate
 
@@ -19,15 +19,15 @@
             openURL:(NSURL *)url
             options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
 {
-    return [RCTLinkingManager application:application openURL:url options:options];
+  return [RCTLinkingManager application:application openURL:url options:options];
 }
 
 - (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
  restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
 {
-    return [RCTLinkingManager application:application
-                     continueUserActivity:userActivity
-                       restorationHandler:restorationHandler];
+  return [RCTLinkingManager application:application
+                   continueUserActivity:userActivity
+                     restorationHandler:restorationHandler];
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -49,11 +49,11 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-  #if DEBUG
-    return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-  #else
-    return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-  #endif
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
 }
 
 - (NSArray<id<RCTBridgeModule>> *)extraModulesForBridge:(RCTBridge *)bridge {
@@ -62,43 +62,35 @@
 
 // hide snapshot in task switcher
 - (void)applicationWillResignActive:(UIApplication *)application {
+  UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
+  UIVisualEffectView *blurEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
   
-    // prevent from showing blur view if user is purchasing
-    // if([InAppPurchaseModule isUserPurchasing]){
-    //      return;
-    // }
-    
-    self.window.backgroundColor = [UIColor clearColor];
-
-    UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
-    UIVisualEffectView *blurEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
-    blurEffectView.frame = self.window.bounds;
-    blurEffectView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-
-    blurEffectView.tag = 1234;
-    blurEffectView.alpha = 0;
-    [self.window addSubview:blurEffectView];
-    [self.window bringSubviewToFront:blurEffectView];
-
-    // fade in the view
-    [UIView animateWithDuration:0.5 animations:^{
-        blurEffectView.alpha = 1;
-    }];
+  NSPredicate *isKeyWindow = [NSPredicate predicateWithFormat:@"isKeyWindow == YES"];
+  UIWindow *topView = [[[UIApplication sharedApplication] windows] filteredArrayUsingPredicate:isKeyWindow].firstObject;
   
-    blureViewActive = YES;
+  blurEffectView.frame = topView.frame;
+  blurEffectView.tag = 3549886;
+  blurEffectView.alpha = 0;
+  [topView addSubview:blurEffectView];
+  [UIView animateWithDuration:0.5 animations:^{
+    blurEffectView.alpha = 1;
+  }];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // grab a reference to our custom blur view
-    UIView *blurEffectView = [self.window viewWithTag:1234];
-
+  NSPredicate *isKeyWindow = [NSPredicate predicateWithFormat:@"isKeyWindow == YES"];
+  UIWindow *topView = [[[UIApplication sharedApplication] windows] filteredArrayUsingPredicate:isKeyWindow].firstObject;
+  
+  UIView *blurEffectView = [topView viewWithTag:3549886];
+  if (blurEffectView){
     // fade away colour view from main view
     [UIView animateWithDuration:0.5 animations:^{
-        blurEffectView.alpha = 0;
+      blurEffectView.alpha = 0;
     } completion:^(BOOL finished) {
-        // remove when finished fading
-        [blurEffectView removeFromSuperview];
+      // remove when finished fading
+      [blurEffectView removeFromSuperview];
     }];
+  }
 }
 
 - (BOOL)application:(UIApplication *)application shouldAllowExtensionPointIdentifier:(NSString *)extensionPointIdentifier
