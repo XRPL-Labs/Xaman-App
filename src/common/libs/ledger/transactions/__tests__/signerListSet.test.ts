cba3050fab53db3af7dbf63b2b4a4dc15a89d392
N3TC4T <netcat.av@gmail.com>
2022-06-22 12:40:40 +0200

    fix: unit tests

--- src/common/libs/ledger/transactions/__tests__/signerListSet.test.ts
+++ src/common/libs/ledger/transactions/__tests__/signerListSet.test.ts
@@ -19,9 +19,13 @@ describe('SignerListSet tx', () => {
         expect(instance.SignerQuorum).toBe(3);
 
         expect(instance.SignerEntries).toStrictEqual([
-            { account: 'rsA2LpzuawewSBQXkiju3YQTMzW13pAAdW', weight: 2 },
-            { account: 'rUpy3eEg8rqjqfUoLeBnZkscbKbFsKXC3v', weight: 1 },
-            { account: 'raKEEVSGnKSD9Zyvxu4z6Pqpm4ABH8FS6n', weight: 1 },
+            {
+                account: 'rsA2LpzuawewSBQXkiju3YQTMzW13pAAdW',
+                walletLocator: '03075F65D8353E3A5DA3193FF976BC17A2D0B9376BE7DA942349B6526E5A2BBF54',
+                weight: 2,
+            },
+            { account: 'rUpy3eEg8rqjqfUoLeBnZkscbKbFsKXC3v', walletLocator: undefined, weight: 1 },
+            { account: 'raKEEVSGnKSD9Zyvxu4z6Pqpm4ABH8FS6n', walletLocator: undefined, weight: 1 },
         ]);
     });
 });
