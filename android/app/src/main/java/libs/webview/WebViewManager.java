4a0280dc5bd8ef5fa8085efc5f0e02ce06d22e79
N3TC4T <netcat.av@gmail.com>
2023-04-28 10:51:41 +0200

    chore(revert): add OTT header to the xapp

--- android/app/src/main/java/libs/webview/WebViewManager.java
+++ android/app/src/main/java/libs/webview/WebViewManager.java
@@ -1,6 +1,5 @@
 package libs.webview;
 
-
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -151,11 +150,10 @@ public class WebViewManager extends SimpleViewManager<WebView> {
     protected static final int SHOULD_OVERRIDE_URL_LOADING_TIMEOUT = 250;
     protected WebViewConfig mWebViewConfig;
     protected RNCWebChromeClient mWebChromeClient = null;
-    protected @Nullable String mUserAgent = null;
-    protected @Nullable String mUserAgentWithApplicationName = null;
-
-    protected static final String ottHeaderField = "X-OTT";
-    protected static @Nullable String ottHeaderValue = null;
+    protected @Nullable
+    String mUserAgent = null;
+    protected @Nullable
+    String mUserAgentWithApplicationName = null;
 
 
     public WebViewManager() {
@@ -246,6 +244,7 @@ public class WebViewManager extends SimpleViewManager<WebView> {
     }
 
 
+
     @ReactProp(name = "messagingModuleName")
     public void setMessagingModuleName(WebView view, String moduleName) {
         ((RNCWebView) view).setMessagingModuleName(moduleName);
@@ -319,20 +318,13 @@ public class WebViewManager extends SimpleViewManager<WebView> {
                             }
                         } else {
                             headerMap.put(key, headers.getString(key));
-
-                            // set OTT value if present in the header
-                            if (ottHeaderField.equals(key)) {
-                                ottHeaderValue = headers.getString(key);
-                            }
                         }
                     }
                 }
-
                 view.loadUrl(url, headerMap);
                 return;
             }
         }
-
         view.loadUrl(BLANK_URL);
     }
 
@@ -515,13 +507,33 @@ public class WebViewManager extends SimpleViewManager<WebView> {
                             createWebViewEvent(webView, url)));
         }
 
+
+//        @Override
+//        public WebResourceResponse shouldInterceptRequest(WebView view,
+//                                                          WebResourceRequest request) {
+//            if (!request.getUrl().getScheme().contains("http"))
+//                return null;
+//
+//            String host = request.getUrl().getHost();
+//            if (host.contains("googletagmanager") || host.contains("google-analytics") || host.contains("sentry")) {
+//                InputStream stream = new InputStream() {
+//                    @Override
+//                    public int read() throws IOException {
+//                        return -1;
+//                    }
+//                };
+//                WebResourceResponse resp = new WebResourceResponse("text/html", "utf-8", stream);
+//                resp.setStatusCodeAndReasonPhrase(400, "Bad request");
+//                return resp;
+//            }
+//
+//            return null;
+//        }
+
         @Override
-        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
             final RNCWebView rncWebView = (RNCWebView) view;
             final boolean isJsDebugging = ((ReactContext) view.getContext()).getJavaScriptContextHolder().get() == 0;
-            final String url = request.getUrl().toString();
-
-            boolean shouldOverride;
 
             if (!isJsDebugging && rncWebView.mCatalystInstance != null) {
                 final Pair<Integer, AtomicReference<ShouldOverrideCallbackState>> lock = WebViewModule.shouldOverrideUrlLoadingLock.getNewLock();
@@ -551,8 +563,11 @@ public class WebViewManager extends SimpleViewManager<WebView> {
                     return false;
                 }
 
-                shouldOverride = lockObject.get() == ShouldOverrideCallbackState.SHOULD_OVERRIDE;
+                final boolean shouldOverride = lockObject.get() == ShouldOverrideCallbackState.SHOULD_OVERRIDE;
                 WebViewModule.shouldOverrideUrlLoadingLock.removeLock(lockIdentifier);
+
+                return shouldOverride;
+
             } else {
                 FLog.w(TAG, "Couldn't use blocking synchronous call for onShouldStartLoadWithRequest due to debugging or missing Catalyst instance, falling back to old event-and-load.");
                 progressChangedFilter.setWaitingForCommandLoadUrl(true);
@@ -561,25 +576,14 @@ public class WebViewManager extends SimpleViewManager<WebView> {
                         new TopShouldStartLoadWithRequestEvent(
                                 view.getId(),
                                 createWebViewEvent(view, url)));
-                shouldOverride = true;
+                return true;
             }
+        }
 
-            // if we can load the request, we check if the OTT header is already present in the request
-            // add it if it's not necessary
-            if (
-                    !shouldOverride && ottHeaderValue != null &&
-                            (
-                                    request.getRequestHeaders() == null || !request.getRequestHeaders().containsKey(ottHeaderField)
-                            )
-            ) {
-                Map<String, String> headers = new HashMap<>();
-                headers.put(ottHeaderField, ottHeaderValue);
-                view.loadUrl(url, headers);
-                shouldOverride = true;
-            }
-
-
-            return shouldOverride;
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            final String url = request.getUrl().toString();
+            return this.shouldOverrideUrlLoading(view, url);
         }
 
         @Override
@@ -866,9 +870,9 @@ public class WebViewManager extends SimpleViewManager<WebView> {
                 ArrayList<String> processedPermissions = new ArrayList<>();
                 for (String permission : grantedPermissions) {
                     String permissionName = permission;
-                    if (permission.equals(PermissionRequest.RESOURCE_AUDIO_CAPTURE)) {
+                    if(permission.equals(PermissionRequest.RESOURCE_AUDIO_CAPTURE)){
                         permissionName = "Microphone";
-                    } else if (permission.equals(PermissionRequest.RESOURCE_VIDEO_CAPTURE)) {
+                    }else if(permission.equals(PermissionRequest.RESOURCE_VIDEO_CAPTURE)){
                         permissionName = "Camera";
                     }
                     AlertDialog.Builder builder = new AlertDialog.Builder(mReactContext);
