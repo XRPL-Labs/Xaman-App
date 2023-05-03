22eb61198ec452bd77d370d991a4eea757c9ade6
N3TC4T <netcat.av@gmail.com>
2023-05-03 14:11:00 +0200

    feat: add shadow color to xApp category select

--- src/components/Modules/XAppStore/CategorySelect/styles.ts
+++ src/components/Modules/XAppStore/CategorySelect/styles.ts
@@ -4,11 +4,16 @@ import { AppSizes } from '@theme';
 /* Styles ==================================================================== */
 export default StyleService.create({
     container: {
+        backgroundColor: '$background',
         flexGrow: 0,
         flexDirection: 'row',
         paddingVertical: AppSizes.paddingSml,
+        shadowColor: '$background',
+        shadowOffset: { width: 0, height: 5 },
+        shadowRadius: 5,
+        shadowOpacity: 1,
+        zIndex: 9999,
     },
-    contentContainer: {},
     button: {
         borderRadius: AppSizes.scale(75) / 10,
         marginRight: AppSizes.paddingExtraSml,
