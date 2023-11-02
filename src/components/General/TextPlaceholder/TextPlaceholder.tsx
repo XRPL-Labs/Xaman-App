d9d194a6991f4dc0a558b37ccc625126c619eaeb
N3TC4T <netcat.av@gmail.com>
2023-11-02 12:38:00 +0100

    feat: TextPlaceholder add fadeIn animation when visible

--- src/components/General/TextPlaceholder/TextPlaceholder.tsx
+++ src/components/General/TextPlaceholder/TextPlaceholder.tsx
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { Text, Animated, InteractionManager, TextStyle } from 'react-native';
+import { Text, Animated, InteractionManager, TextStyle, View } from 'react-native';
 
 import { TouchableDebounce } from '@components/General/TouchableDebounce';
 
@@ -17,7 +17,8 @@ export interface Props {
 
 /* component ==================================================================== */
 class TextPlaceholder extends PureComponent<Props> {
-    private readonly animatedFade: Animated.Value;
+    private readonly animatedPulse: Animated.Value;
+    private readonly animatedFadeIn: Animated.Value;
 
     static defaultProps = {
         length: 12,
@@ -25,7 +26,9 @@ class TextPlaceholder extends PureComponent<Props> {
 
     constructor(props: Props) {
         super(props);
-        this.animatedFade = new Animated.Value(0.3);
+
+        this.animatedPulse = new Animated.Value(0.3);
+        this.animatedFadeIn = new Animated.Value(props.isLoading ? 0.3 : 1);
     }
 
     componentDidMount() {
@@ -35,31 +38,45 @@ class TextPlaceholder extends PureComponent<Props> {
     componentDidUpdate(prevProps: Readonly<Props>) {
         const { isLoading } = this.props;
 
+        // start the pulse animation
         if (!prevProps.isLoading && isLoading) {
             InteractionManager.runAfterInteractions(this.startPlaceholderAnimation);
+        }
+
+        // start the pulse animation
+        if (prevProps.isLoading && !isLoading) {
+            InteractionManager.runAfterInteractions(this.startFadeInAnimation);
         }
     }
 
     startPlaceholderAnimation = () => {
         const { isLoading } = this.props;
 
-        // if app provided stop the placeholder animation
+        // if not loading anymore then show the
         if (!isLoading) {
             return;
         }
 
         Animated.sequence([
-            Animated.timing(this.animatedFade, {
+            Animated.timing(this.animatedPulse, {
                 toValue: 0.1,
                 duration: 1000,
                 useNativeDriver: true,
             }),
-            Animated.timing(this.animatedFade, {
+            Animated.timing(this.animatedPulse, {
                 toValue: 0.3,
                 duration: 1000,
                 useNativeDriver: true,
             }),
         ]).start(this.startPlaceholderAnimation);
+    };
+
+    startFadeInAnimation = () => {
+        Animated.timing(this.animatedFadeIn, {
+            toValue: 1,
+            duration: 250,
+            useNativeDriver: true,
+        }).start();
     };
 
     onPress = () => {
@@ -71,25 +88,32 @@ class TextPlaceholder extends PureComponent<Props> {
     };
 
     render() {
-        const { style, isLoading, length, numberOfLines, selectable, children } = this.props;
+        const { style, onPress, isLoading, length, numberOfLines, selectable, children } = this.props;
 
         if (isLoading) {
             return (
                 <Animated.Text
                     numberOfLines={numberOfLines}
-                    style={[style, styles.placeholder, { opacity: this.animatedFade }]}
+                    style={[style, styles.placeholder, { opacity: this.animatedPulse }]}
                 >
                     {'\u00A0'.repeat(length)}
                 </Animated.Text>
             );
         }
 
+        const isPressable = typeof onPress === 'function';
+
+        const ContainerComponent = Animated.createAnimatedComponent(isPressable ? TouchableDebounce : View);
+        const ContainerProps = isPressable ? { activeOpacity: 0.8, onPress: this.onPress } : {};
+
         return (
-            <TouchableDebounce activeOpacity={0.8} onPress={this.onPress}>
+            // @ts-ignore
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            <ContainerComponent {...ContainerProps} style={{ opacity: this.animatedFadeIn }}>
                 <Text selectable={selectable} numberOfLines={numberOfLines} style={style}>
                     {children}
                 </Text>
-            </TouchableDebounce>
+            </ContainerComponent>
         );
     }
 }
