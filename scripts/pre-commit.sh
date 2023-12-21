faca03b7c2d3ade1662380997eec3fbde329cdbc
N3TC4T <netcat.av@gmail.com>
2023-12-21 14:52:14 +0100

    chore: optimize pre-commit.sh

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,34 +1,35 @@
 #!/usr/bin/env bash
-files=$(git diff --cached --name-only --diff-filter=ACM | grep -E '.js$|.ts$|.tsx$')
-
+files=$(git diff --cached --name-only --diff-filter=ACM | grep -E  '\.(js|ts|tsx)$')
 if [ -z "$files" ]; then
     exit 0
 fi
 
-echo "Checking lint for:"
-for f in $files; do
-    echo "$f"
-    e=$(node_modules/.bin/eslint --quiet --fix $f)
-    if [[ -n "$e" ]]; then
-        echo "ERROR: Check eslint hints."
-        echo "$e"
-        exit 1 # reject
-    fi
-done
+# check for any linting errors
+lintError=$(node_modules/.bin/eslint --quiet $files)
+if [[ -n "$lintError" ]]; then
+    echo "ERROR: Check eslint hints."
+    echo "$lintError"
+    exit 1
+fi
 
-echo "Checking for TSC"
-tsc=$(node_modules/.bin/tsc --noEmit)
-if [[ -n "$tsc" ]]; then
-    echo "ERROR: Check TSC hints."
-    echo "$tsc"
-    exit 1 # reject
+# check tsc for only relevant files
+typeScriptFiles=$(echo $files | tr ' ' '\n' | grep -E '\.(ts|tsx)$')
+if [[ -z "$typeScriptFiles" ]]; then
+    echo "No TypeScript files changed. Skipping TSC check."
+else
+    tscError=$(node_modules/.bin/tsc --noEmit)
+    if [[ -n "$tscError" ]]; then
+        echo "ERROR: Check TSC hints."
+        echo "$tscError"
+        exit 1
+    fi
 fi
 
 
-echo "Checking for Translations"
-translations=$(node scripts/sync-locales.js check)
-if [[ -n "$translations" ]]; then
+# checking for any translations mismatch
+translationsError=$(node scripts/sync-locales.js check)
+if [[ -n "$translationsError" ]]; then
     echo "ERROR: Check Translations hints."
-    echo "$translations"
+    echo "$translationsError"
     exit 1 # reject
 fi
