9e1c346e782480d6ac37751f35c44d74189386c8
N3TC4T <netcat.av@gmail.com>
2024-10-09 10:29:02 +0200

    fix: improve error handling for realm key data decryption

--- src/app.tsx
+++ src/app.tsx
@@ -100,6 +100,7 @@ class Application {
         if (message) {
             if (
                 message.indexOf('Realm file decryption failed') > -1 ||
+                message.indexOf('Could not decrypt data') > -1 ||
                 message.indexOf('Could not decrypt bytes') > -1
             ) {
                 Alert.alert('Error', ErrorMessages.storageDecryptionFailed, [
