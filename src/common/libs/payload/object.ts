8a12b1239dbc8048c40a88bf9ce07829faa23a61
N3TC4T <netcat.av@gmail.com>
2024-10-24 11:42:30 +0200

    feat: allow fallback transactions to all users

--- src/common/libs/payload/object.ts
+++ src/common/libs/payload/object.ts
@@ -7,8 +7,6 @@ import { Endpoints } from '@common/constants/endpoints';
 import ApiService, { ApiError } from '@services/ApiService';
 import LoggerService from '@services/LoggerService';
 
-import CoreRepository from '@store/repositories/core';
-
 import { TransactionFactory } from '@common/libs/ledger/factory';
 
 import Localize from '@locale';
@@ -328,20 +326,13 @@ export class Payload {
         }
 
         // check if normal transaction and supported by the app
-        // NOTE: only in case of developer mode enabled we allow transaction fallback
         if (
             request_json.TransactionType &&
             !Object.values(TransactionTypes).includes(request_json.TransactionType as TransactionTypes)
         ) {
-            if (CoreRepository.isDeveloperModeEnabled()) {
-                logger.warn(
-                    `Requested transaction type "${request_json.TransactionType}" not found, revert to fallback transaction.`,
-                );
-            } else {
-                throw new Error(
-                    `Requested transaction type "${request_json.TransactionType} is not supported at the moment.`,
-                );
-            }
+            logger.warn(
+                `Requested transaction type "${request_json.TransactionType}" not found, revert to fallback transaction.`,
+            );
         }
 
         let craftedTransaction;
