1df81f26eae60215f0ed7318ce53cceb647c62a9
Wietse Wind <mail@wietse.com>
2025-01-24 01:57:39 +0100

    Update JSON decode error msg

--- src/services/__tests__/ApiService.test.ts
+++ src/services/__tests__/ApiService.test.ts
@@ -71,7 +71,7 @@ describe('API', () => {
 
         it(`call with invalid json response`, async () => {
             await expect(ApiService.fetch(Endpoints.Ping, 'GET', { action: 'invalid_json' })).rejects.toMatchObject(
-                new ApiError('Response returned is not valid JSON Unexpected token i in JSON at position 0'),
+                new ApiError('Response returned is not valid JSON Unexpected token \'i\', \"invalid_json\" is not valid JSON'),
             );
         });
 
